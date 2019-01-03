7e56e12ea765bb4c7b18caa355900fe2aa54bbe1
msidhoum <mehdi.sidhoum@gmail.com>
2019-01-03 18:22:11 +0100

    chore(*): add missing files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'settings', component: SettingsComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { MaterialModule } from './components/material/material.module';
+import { GridsterModule } from 'angular-gridster2';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { DynamicModule } from 'ng-dynamic-component';
+import { DynamicComponent } from './components/dynamic/dynamic.component';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
+import { WeatherMapComponent } from './components/weather-map/weather-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SettingsComponent,
+    DynamicComponent,
+    WeatherCardComponent,
+    WeatherMapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+    GridsterModule,
+    FlexLayoutModule,
+    DynamicModule.withComponents([WeatherCardComponent, WeatherMapComponent])
   ],
   providers: [],
   bootstrap: [AppComponent]
